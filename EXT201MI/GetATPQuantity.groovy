bdef32d4b081926d035a23b940db77a238685f23
Suriya Narayanan <88660776+Suriya-Fortude@users.noreply.github.com>
2023-12-12 20:20:42 +0530

    Add files via upload

---
+++ EXT201MI/GetATPQuantity.groovy
@@ -0,0 +1,117 @@
+/**
+ * README
+ * This extension is being used to Get records from EXTPQT table. 
+ *
+ * Name: EXT201MI.GetATPQuantity
+ * Description: Get records from EXTPQT table
+ * Date	      Changed By                      Description
+ *20230407  SuriyaN@fortude.co     Get records from EXTPQT table
+ *
+ */
+
+
+public class GetATPQuantity extends ExtendM3Transaction {
+  private final MIAPI mi
+  private final DatabaseAPI database
+  private final ProgramAPI program
+  private final MICallerAPI miCaller
+
+  private String iWHLO, iITNO
+  private int iCONO
+  private boolean validInput = true
+
+  public GetATPQuantity(MIAPI mi, DatabaseAPI database, ProgramAPI program, MICallerAPI miCaller) {
+    this.mi = mi
+    this.database = database
+    this.program = program
+    this.miCaller = miCaller
+  }
+  /**
+   ** Main function
+   * @param
+   * @return
+   */
+  public void main() {
+    iWHLO = (mi.inData.get("WHLO") == null || mi.inData.get("WHLO").trim().isEmpty()) ? "" : mi.inData.get("WHLO")
+    iITNO = (mi.inData.get("ITNO") == null || mi.inData.get("ITNO").trim().isEmpty()) ? "" : mi.inData.get("ITNO")
+
+    iCONO = (mi.inData.get("CONO") == null || mi.inData.get("CONO").trim().isEmpty()) ? program.LDAZD.CONO as Integer : mi.inData.get("CONO") as Integer
+    
+    validateInput()
+    if (validInput) {
+    getRecord()
+    }
+  }
+  
+     /**
+   *Validate records 
+   * @params 
+   * @return 
+   */
+  public validateInput() {
+    //Validate Company Number
+    Map<String, String> params = ["CONO": iCONO.toString().trim()]
+    Closure<?>  callback = {
+      Map < String,
+      String > response ->
+      if (response.CONO == null) {
+        mi.error("Invalid Company Number " + iCONO)
+        validInput = false
+        return false
+      }
+    }
+    miCaller.call("MNS095MI", "Get", params, callback)
+
+    //Validate Item Number
+    params = ["CONO": iCONO.toString().trim(),"ITNO":iITNO.toString().trim()]
+    callback = {
+      Map < String,
+        String > response ->
+        if (response.ITNO == null) {
+          mi.error("Invalid Item Number " + iITNO)
+          validInput = false
+          return false
+        }
+    }
+    
+    miCaller.call("MMS200MI", "Get", params, callback)
+    
+     //Validate Warehouse Number
+      params = ["CONO": iCONO.toString().trim(),"WHLO":iWHLO.toString().trim()]
+      callback = {
+        Map < String,
+          String > response ->
+          if (response.WHLO == null) {
+            mi.error("Invalid Warehouse Number " + iWHLO)
+            validInput = false
+            return false
+          }
+      }
+      miCaller.call("MMS005MI", "GetWarehouse", params, callback)
+
+  }
+  /**
+   *Get records from EXTPQT table
+   * @params 
+   * @return 
+   */
+  public getRecord() {
+    DBAction query = database.table("EXTPQT").selection("EXWHLO", "EXITNO", "EXUPDS","EXVAL9").index("00").build()
+    DBContainer container = query.getContainer()
+
+    container.set("EXCONO", iCONO)
+    container.set("EXWHLO", iWHLO)
+    container.set("EXITNO", iITNO)
+    if (query.read(container)) {
+      mi.outData.put("WHLO", container.get("EXWHLO").toString())
+      mi.outData.put("ITNO", container.get("EXITNO").toString())
+      mi.outData.put("UPDS", container.get("EXUPDS").toString())
+      mi.outData.put("VAL9", container.get("EXVAL9").toString())
+      mi.write()
+
+    } else {
+      mi.error("Record does not Exist.")
+      return
+    }
+  }
+}
